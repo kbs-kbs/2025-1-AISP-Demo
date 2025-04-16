837faa76b15d9626ed52c288874c47a58fa7ba2e
kbs-kbs <yaglus58@gmail.com>
2025-04-16 11:42:36 +0900

    update

--- home/run_home.py
+++ home/run_home.py
@@ -1,74 +1,196 @@
 import streamlit as st
 import pandas as pd
 import module.watcha_pedia_crawler.rankings_crawler as wc
+from datetime import datetime
 
-# @st.cache_data(ttl=3600)
+@st.cache_data(ttl=3600)
 def get_cached_data():
     wc.crawl()
+    update_time = datetime.now().strftime('%Y-%m-%d %H:%M')
     return [
         pd.read_csv("csv/box_office_ranking.csv"),
         pd.read_csv("csv/watcha_ranking.csv"),
-        pd.read_csv("csv/netflix_ranking.csv")
+        pd.read_csv("csv/netflix_ranking.csv"),
+        update_time
     ]
 
 def run_home():
     (
         box_office_ranking_df,
         watcha_ranking_df,
-        netflix_ranking_df
+        netflix_ranking_df,
+        update_time
     ) = get_cached_data()
 
+    st.markdown(
+        """
+        <style>
+        .movie-rank {
+            font-weight: bold;
+        }
+
+        .movie-title {
+            font-weight: bold;
+        }
+
+        .field-name {
+            margin-bottom: 0.25rem;
+            font-size: 12px !important;
+            color: gray;
+        }
+
+        .field-value {
+            margin-bottom: 2rem;
+        }
+        
+        .st-key-box_office_ranking_update_button > div > button {
+            float: right;
+            min-height: initial;
+        }
+        .st-key-box_office_ranking_update_button > div > button > div > p {
+            font-size: 12px !important;
+        }
+
+        .st-key-watcha_ranking_update_button > div > button {
+            float: right;
+            min-height: initial;
+        }
+        .st-key-watcha_ranking_update_button > div > button > div > p {
+            font-size: 12px !important;
+        }
+
+        .st-key-netflix_ranking_update_button > div > button {
+            float: right;
+            min-height: initial;
+        }
+        .st-key-netflix_ranking_update_button > div > button > div > p {
+            font-size: 12px !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     st.markdown('# 박스 오피스 순위 Top 10')
+    if st.button(f"마지막 업데이트: {update_time} 🔄", key='box_office_ranking_update_button'):
+        get_cached_data.clear()
+        st.rerun()
     for row in range(0, 10, 5):  # 0-4, 5-9
         cols = st.columns(5)
         for idx in range(5):
             data = box_office_ranking_df.iloc[row + idx]
             with cols[idx]:
-                st.write(data['rank'])
+                st.markdown(
+                    f'<p class="movie-rank">{data['rank']}위</p>', 
+                    unsafe_allow_html=True
+                )
                 st.image(data['image_url'])
-                st.write(data['title'])
-
-                # 개봉년도/국가 분할
-                release_year, country = st.columns(2)
-                release_year.write(str(data['release_year']))
-                country.write(data['country'])
+                st.markdown(
+                    f'<p class="movie-title">{data["title"]}</p>', 
+                    unsafe_allow_html=True
+                )
 
                 # 예매율/관객수 분할
                 reservation, audience = st.columns(2)
-                reservation.write(data['reservation'])
-                audience.write(data['audience'])
+                with reservation:
+                    st.markdown(
+                        '<p class="field-name">예매율</p>', 
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(
+                        f'<p class="field-value">{data['reservation']}</p>', 
+                        unsafe_allow_html=True
+                    )
+                with audience:
+                    st.markdown(
+                        '<p class="field-name">관객 수</p>', 
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(
+                        f'<p class="field-value">{data['audience']}</p>', 
+                        unsafe_allow_html=True
+                    )
     
 
     st.markdown('# 왓챠 영화 순위 Top 10')
+    if st.button(f"마지막 업데이트: {update_time} 🔄", key='watcha_ranking_update_button'):
+        get_cached_data.clear()
+        st.rerun()
     for row in range(0, 10, 5):  # 0-4, 5-9
         cols = st.columns(5)
         for idx in range(5):
             data = watcha_ranking_df.iloc[row + idx]
             with cols[idx]:
-                st.write(data['rank'])
+                st.markdown(
+                    f'<p class="movie-rank">{data['rank']}위</p>', 
+                    unsafe_allow_html=True
+                )
                 st.image(data['image_url'])
-                st.write(data['title'])
+                st.markdown(
+                    f'<p class="movie-title">{data["title"]}</p>', 
+                    unsafe_allow_html=True
+                )
 
                 # 개봉년도/국가 분할
                 release_year, country = st.columns(2)
-                release_year.write(str(data['release_year']))
-                country.write(data['country'])
+                with release_year:
+                    st.markdown(
+                        '<p class="field-name">개봉 연도</p>', 
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(
+                        f'<p class="field-value">{data['release_year']}</p>', 
+                        unsafe_allow_html=True
+                    )
+                with country:
+                    st.markdown(
+                        '<p class="field-name">국가</p>', 
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(
+                        f'<p class="field-value">{data['country']}</p>', 
+                        unsafe_allow_html=True
+                    )
 
     st.markdown('# 넷플릭스 영화 순위 Top 10')
+    if st.button(f"마지막 업데이트: {update_time} 🔄", key='netflix_ranking_update_button'):
+        get_cached_data.clear()
+        st.rerun()
     for row in range(0, 10, 5):  # 0-4, 5-9
         cols = st.columns(5)
         for idx in range(5):
             data = netflix_ranking_df.iloc[row + idx]
             with cols[idx]:
-                st.write(data['rank'])
+                st.markdown(
+                    f'<p class="movie-rank">{data['rank']}위</p>', 
+                    unsafe_allow_html=True
+                )
                 st.image(data['image_url'])
-                st.write(data['title'])
+                st.markdown(
+                    f'<p class="movie-title">{data["title"]}</p>', 
+                    unsafe_allow_html=True
+                )
 
                 # 개봉년도/국가 분할
                 release_year, country = st.columns(2)
-                release_year.write(str(data['release_year']))
-                country.write(data['country'])
-
+                with release_year:
+                    st.markdown(
+                        '<p class="field-name">개봉 연도</p>', 
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(
+                        f'<p class="field-value">{data['release_year']}</p>', 
+                        unsafe_allow_html=True
+                    )
+                with country:
+                    st.markdown(
+                        '<p class="field-name">국가</p>', 
+                        unsafe_allow_html=True
+                    )
+                    st.markdown(
+                        f'<p class="field-value">{data['country']}</p>', 
+                        unsafe_allow_html=True
+                    )
 
     st.dataframe(box_office_ranking_df)
     st.dataframe(watcha_ranking_df)
--- scrpy_plwrt_demo/items.py
+++ scrpy_plwrt_demo/items.py
@@ -14,4 +14,18 @@ class ThumbnailItem(scrapy.Item):
     reservation=scrapy.Field()
     audience=scrapy.Field()
     image_url=scrapy.Field()
+    url=scrapy.Field()
+
+class MovieItem(scrapy.Item):
+    title=scrapy.Field()
+    en_title=scrapy.Field()
+    release_year=scrapy.Field()
+    country=scrapy.Field()
+    genre=scrapy.Field()
+    running_time=scrapy.Field()
+    rating=scrapy.Field()
+    description=scrapy.Field()
+    cast=scrapy.Field()
+    image_url=scrapy.Field()
+    url=scrapy.Field()
 
