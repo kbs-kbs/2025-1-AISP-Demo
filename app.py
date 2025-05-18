25a54fdd5be48988fe909c2b07aa3b5e1a0384de
kbs-kbs <yaglus58@gmail.com>
2025-05-18 15:44:27 +0900

    update

--- app.py
+++ app.py
@@ -7,13 +7,59 @@ if "selected_movie_id" not in st.session_state:
 
 def main():
     st.set_page_config(page_title="캡슐러", layout='wide')
-    with st.sidebar:
-        selected = option_menu(
-            "메뉴", ["홈", "북마크"],
-            icons=["house", "bookmark"],
-            menu_icon="list", default_index=0
-        )
-        
+    st.markdown("""
+    <style>
+    #stDecoration {
+        display: none;
+    }
+    .stAppHeader {
+        height: 4.25rem;
+    }
+    #root > div:nth-of-type(1) > div.withScreencast > div > div > section > div.stMainBlockContainer > div > div > div > div:nth-of-type(1) {
+        display: none;
+    }
+    #root > div:nth-of-type(1) > div.withScreencast > div > div > section > div.stMainBlockContainer > div > div > div > div:nth-of-type(2) {
+        position: fixed;
+        top: 1.25rem;
+        left: 50%;
+        transform: translateX(-50%);
+        z-index: 9999990;
+    }
+    [data-testid="stCustomComponentV1"] {
+        width: auto;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    st.logo("images/logo-default.svg", size="large")
+    selected = option_menu(
+        None, ["홈", "북마크"],
+        icons=[" ", " "],
+        default_index=0,
+        orientation="horizontal",
+        styles={
+            "container": {
+                "padding": 0,
+                "background-color": "unset",
+                "border-radius": "unset",
+            },
+            "nav-link-selected": {
+                "margin": "0",
+                "padding": "0.25rem",
+                "background-color": "unset",
+                "border-radius": "unset",
+                "font-size": "0.875rem",
+                "font-weight": "bold",
+                "color": "var(--primary-color)"
+            },
+            "nav-link": {
+                "margin": "0",
+                "padding": "0.25rem",
+                "font-size": "0.875rem",
+                "font-weight": "bold",
+                "color": "gray"
+            },
+        }
+    )
     if selected == "홈":
         run_home()
     elif selected == "북마크":
